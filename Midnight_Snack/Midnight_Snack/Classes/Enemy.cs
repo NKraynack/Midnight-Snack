56b7dbaa0e300e04de6f9b052df7823b59de2ee7
PikachuXD <purasle@gmail.com>
2015-03-22 22:04:02 -0400

    Added enemy class
    Basic framework; mostly to test git and to get a framework set up for
    this project

---
+++ Midnight_Snack/Midnight_Snack/Classes/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Midnight_Snack.Classes
+{
+    class Enemy : MobileUnit 
+    {
+        private static Enemy instance = new Enemy(new Vector2(0, 0), 100, 100, 0, 0, 3, 10);
+        
+        public Enemy(Vector2 pos, int width, int height, int row, int col, int range, int health) 
+            : base(pos, width, height, row, col, range, health)
+        {
+            
+        }
+        
+        public static Enemy GetInstance()
+        {
+            return instance;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);   
+        }
+    }
+}
