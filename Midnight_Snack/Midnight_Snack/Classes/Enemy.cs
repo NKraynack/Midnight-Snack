cf574f5cdc8d3ed5dcb396f61b2f6015464a2551
PikachuXD <purasle@gmail.com>
2015-04-02 11:32:06 -0400

    glitchy movement
    when the level is reset the enemy movement is reset to its last position
    instead of its initial position.

--- Midnight_Snack/Midnight_Snack/Classes/Enemy.cs
+++ Midnight_Snack/Midnight_Snack/Classes/Enemy.cs
@@ -14,11 +14,11 @@ namespace Midnight_Snack
     {
 
         Player player = Player.GetInstance();
-
+        private char[,] map_grid; //The grid for the map to generate shortest path
         public Enemy(Vector2 pos, int width, int height, int row, int col, int range, int health, Map map) 
             : base(pos, width, height, row, col, range, health, map)
         {
-            
+            this.map_grid = map.GenerateMapGrid();
         }
 
         public override void LoadContent(ContentManager content)
@@ -62,11 +62,132 @@ namespace Midnight_Snack
                         //insert attack method
                         Attack(player);
                     }
+
+                    if (this.GetCol() - 2 > 0) 
+                    {
+                        MapTile dest = map.GetTile(this.GetRow(), this.GetCol() - 2);
+                        if (this.NoObstacles(this.GetCol() - 2, this.GetRow()) && dest.IsPassable())
+                        {
+                            Move(dest.GetPosition(), this.GetCol() - 2, this.GetRow());
+                        }
+                    }
+                    
                     //End enemy's turn
                     hasEndedTurn = true;
                 }
             }
  
+        }
+
+        private bool NoObstacles(int mov_x, int mov_y) //Do dijkstra and then return if movable
+        {
+            Queue<GridPoint> q = new Queue<GridPoint>();
+            List<GridPoint> solution = new List<GridPoint>();
+            HashSet<GridPoint> discovered = new HashSet<GridPoint>();
+            Dictionary<GridPoint, GridPoint> prev = new Dictionary<GridPoint, GridPoint>();
+            GridPoint player_pos = new GridPoint(player.GetCol(), player.GetRow());
+            GridPoint current = player_pos;
+            GridPoint cursor_pos = new GridPoint(mov_x, mov_y);
+            q.Enqueue(current);
+            discovered.Add(current);
+            while (q.Count != 0)
+            {
+                current = q.Dequeue();
+                if (current.Equals(cursor_pos))
+                {
+                    break;
+                }
+                else
+                {
+                    /*System.Diagnostics.Debug.WriteLine("starting");
+                    System.Diagnostics.Debug.WriteLine("cols:" + max_columns);
+                    System.Diagnostics.Debug.WriteLine("rows:" + max_rows);*/
+                    foreach (GridPoint node in getNeighbors(map.GetNumCols(), map.GetNumRows(), current, map_grid))
+                    {
+                        //System.Diagnostics.Debug.WriteLine("looking: " + node.ToString());
+                        if (!discovered.Contains(node))
+                        {
+                            //System.Diagnostics.Debug.WriteLine("adding: " + node.ToString());
+                            q.Enqueue(node);
+                            prev.Add(node, current);
+                            discovered.Add(node);
+                        }
+                    }
+                }
+            }
+            if (!current.Equals(cursor_pos))
+            {
+                return false;
+            }
+            for (GridPoint node = cursor_pos; node != player_pos; prev.TryGetValue(node, out node))
+            {
+                solution.Add(node);
+            }
+            return solution.Count <= player.GetMoveRange();
+        }
+
+        private List<GridPoint> getNeighbors(int x_limit, int y_limit, GridPoint cur_point, char[,] grid)
+        {
+            /*
+            System.Diagnostics.Debug.WriteLine("getting neighbors");
+            System.Diagnostics.Debug.WriteLine("xlimit:" + x_limit);
+            System.Diagnostics.Debug.WriteLine("ylimit" + y_limit);
+            System.Diagnostics.Debug.WriteLine(cur_point.ToString());
+             * */
+            List<GridPoint> neighbors = new List<GridPoint>();
+            int enemy_x = cur_point.getX();
+            int enemy_y = cur_point.getY();
+
+            //bottom right
+            if ((enemy_x + 1 < x_limit) && (enemy_y + 1 < y_limit) && (enemy_x + 1 >= 0) && (enemy_y + 1 >= 0) &&
+                grid[enemy_x + 1, enemy_y + 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x + 1, enemy_y + 1));
+            }
+            //bottom
+            if ((enemy_x < x_limit) && (enemy_y + 1 < y_limit) && (enemy_x >= 0) && (enemy_y + 1 >= 0) &&
+                grid[enemy_x, enemy_y + 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x, enemy_y + 1));
+            }
+            //bottom left
+            if ((enemy_x - 1 < x_limit) && (enemy_y + 1 < y_limit) && (enemy_x - 1 >= 0) && (enemy_y + 1 >= 0) &&
+                grid[enemy_x - 1, enemy_y + 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x - 1, enemy_y + 1));
+            }
+            //left
+            if ((enemy_x - 1 < x_limit) && (enemy_y < y_limit) && (enemy_x - 1 >= 0) && (enemy_y >= 0) &&
+                grid[enemy_x - 1, enemy_y] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x - 1, enemy_y));
+            }
+            //upper left
+            if ((enemy_x - 1 < x_limit) && (enemy_y - 1 < y_limit) && (enemy_x - 1 >= 0) && (enemy_y - 1 >= 0) &&
+                grid[enemy_x - 1, enemy_y - 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x- 1, enemy_y - 1));
+            }
+            //top
+            if ((enemy_x < x_limit) && (enemy_y - 1 < y_limit) && (enemy_x >= 0) && (enemy_y - 1 >= 0) &&
+                grid[enemy_x, enemy_y - 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x, enemy_y - 1));
+            }
+            //top right
+            if ((enemy_x + 1 < x_limit) && (enemy_y - 1 < y_limit) && (enemy_x + 1 >= 0) && (enemy_y - 1 >= 0) &&
+                grid[enemy_x + 1, enemy_y - 1] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x + 1, enemy_y - 1));
+            }
+            //right
+            if ((enemy_x + 1 < x_limit) && (enemy_y < y_limit) && (enemy_x + 1 >= 0) && (enemy_y >= 0) &&
+                grid[enemy_x + 1, enemy_y] != 'x')
+            {
+                neighbors.Add(new GridPoint(enemy_x + 1, enemy_y));
+            }
+            //System.Diagnostics.Debug.WriteLine("neighbors: " + neighbors.Count);
+            return neighbors;
         }
 
         public bool AdjacentToPlayer()
